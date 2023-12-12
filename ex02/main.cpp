ea496fc13cf0c0c3a4e0d3948fb94e13e6033b1e
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-12-12 15:53:09 +0000

    new

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+	std::string var =  "HI THIS IS BRAIN";
+	std::string  *stringPTR = &var;
+	std::string &stringREF = var;
+
+	std::cout << &var << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << "value of var : " << var << std::endl;
+	std::cout << "value of stringPTR : " << *stringPTR << std::endl;
+	std::cout << "value of stringREF : " << stringREF << std::endl;
+
+
+	return 0;
+}
