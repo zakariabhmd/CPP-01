c59f6f5613ecab85f69c1381c98e6bcbcce6e8f0
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-12-14 20:20:13 +0000

    new

---
+++ ex05/Harl.cpp
@@ -0,0 +1,57 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	std::cout << "A wild Harl has appeared!" << std::endl;
+}
+
+Harl::~Harl(void)
+{
+	std::cout << "Wild Harl flew away!" << std::endl;
+}
+
+void	Harl::debug(void)
+{
+    std::cout << "[DEBUG]   ";
+	std::cout << "I love to get extra bacon for my ";
+	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger.";
+	std::cout << " I just love it!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+    std::cout << "[INFO]    ";
+	std::cout << "I cannot believe adding extra bacon cost more money.";
+	std::cout << " You don’t put enough! If you did I would not have to ask";
+	std::cout << " for it!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+    std::cout << "[WARNING] ";
+	std::cout << "I think I deserve to have some extra bacon for free.";
+	std::cout << " I’ve been coming here for years and you just started ";
+	std::cout << "working here last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+    std::cout << "[ERROR]   ";
+	std::cout << "This is unacceptable, I want to speak to the manager now.";
+	std::cout << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	void		(Harl::*ptr_complain[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*ptr_complain[i])();
+			break ;
+		}
+	}
+}
---
+++ ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl h;
+
+	h.complain("DEBUG");
+	h.complain("INFO");
+	h.complain("WARNING");
+	h.complain("ERROR");
+	return (0);
+}
